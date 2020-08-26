33f8c68838d329a2af6f18c7a6e81ea56f43d2bb
Zimo Xiao <xiaozimo@zuggr.com>
2020-08-26 10:58:28 -0500

    typo

--- src/File.ts
+++ src/File.ts
@@ -9,27 +9,31 @@ const unzipper = require('unzipper')
 
 class File {
     clearTmpFiles() {
-        var tmpDir = __dirname + '/data/tmp'
+        var tmpDir = __dirname + '/../data/tmp'
         setInterval(function () {
-            fs.readdir(tmpDir, function (err, files) {
-                files.forEach(function (file, index) {
-                    if (file != '.gitkeep') {
-                        fs.stat(path.join(tmpDir, file), function (err, stat) {
-                            var endTime, now;
-                            if (err) {
-                                return console.error(err)
-                            }
-                            now = new Date().getTime()
-                            endTime = new Date(stat.ctime).getTime() + 3600000
-                            if (now > endTime) {
-                                return rimraf(path.join(tmpDir, file), function (err) {
-                                    //
-                                })
-                            }
-                        })
-                    }
+            try {
+                fs.readdir(tmpDir, function (err, files) {
+                    files.forEach(function (file, index) {
+                        if (file != '.gitkeep') {
+                            fs.stat(path.join(tmpDir, file), function (err, stat) {
+                                var endTime, now;
+                                if (err) {
+                                    return console.error(err)
+                                }
+                                now = new Date().getTime()
+                                endTime = new Date(stat.ctime).getTime() + 3600000
+                                if (now > endTime) {
+                                    return rimraf(path.join(tmpDir, file), function (err) {
+                                        //
+                                    })
+                                }
+                            })
+                        }
+                    })
                 })
-            })
+            } catch {
+                //
+            }
         }, 60 * 60 * 1000)
     }
 
