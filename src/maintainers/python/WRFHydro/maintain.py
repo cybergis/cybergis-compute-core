c310231730fae9564d2693211cf25dd682964538
zhiyuli <zyli2004@gmail.com>
2020-11-04 19:35:54 -0600

    only download output folder

--- src/maintainers/python/WRFHydro/maintain.py
+++ src/maintainers/python/WRFHydro/maintain.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from cybergis import WRFHydroSupervisorToHPC
 
 machine = str(sys.argv[1])
@@ -17,7 +18,7 @@ if status == "C" or status == "UNKNOWN":
     s.download(
         remote_job_folder_path, local_job_folder_path
     )
-    print('@custom_downloaded_path=[' + local_job_folder_path + ']')
+    print('@custom_downloaded_path=[' + os.path.join(local_job_folder_path, "output") + ']')
     print("@event=[JOB_ENDED:WRFHydro job with remote_id " + remote_id + " completed]")
 elif status == "ERROR":
     print("@event=[JOB_FAILED:WRFHydro job with remote_id " + remote_id + " failed]")
