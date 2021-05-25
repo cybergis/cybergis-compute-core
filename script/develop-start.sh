23aa5719980116ce5931b63976016529ea79fbda
Zimo Xiao <xiaozimo@zuggr.com>
2021-05-25 19:23:27 +0800

    update

--- script/develop-start.sh
+++ script/develop-start.sh
@@ -3,12 +3,13 @@ print_usage() {
   echo "-l [OPENCONNECT_URL] -u [OPENCONNECT_USER] -p [OPENCONNECT_PASSWORD] -g [OPENCONNECT_AUTHGROUP]"
 }
 
-while getopts 'u:p:g:l:h' flag; do
+while getopts 'u:p:g:l:hb' flag; do
   case "${flag}" in
     u) export OPENCONNECT_USER="${OPTARG}" ;;
     p) export OPENCONNECT_PASSWORD="${OPTARG}" ;;
     g) export OPENCONNECT_AUTHGROUP="${OPTARG}" ;;
     l) export OPENCONNECT_URL="${OPTARG}" ;;
+    b) export RUN_IN_BACKGROUND="true";;
     h) print_usage
        exit 1 ;;
   esac
@@ -18,4 +19,8 @@ echo "compiling TypeScript..."
 npm run build
 
 echo "running job-supervisor..."
-docker-compose -f ./docker-compose.yml up --remove-orphans job_supervisor
+if [[ ! -z "${RUN_IN_BACKGROUND}" ]]; then
+  docker-compose -f ./docker-compose.yml up -d --remove-orphans job_supervisor
+else
+  docker-compose -f ./docker-compose.yml up --remove-orphans job_supervisor
+fi
--- script/init.sh
+++ script/init.sh
@@ -10,16 +10,16 @@ then
     iptables -D INPUT -p tcp -m tcp --dport 443 -j ACCEPT
     iptables -D INPUT -i eth0 -p tcp -m tcp --dport 22 -j ACCEPT
     iptables -D INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-    iptables -D INPUT -i eth0 -p tcp -m tcp --dport 3000 -j ACCEPT
-    iptables -D INPUT -p tcp -m tcp --dport 3000 -j ACCEPT
+    iptables -D INPUT -i eth0 -p tcp -m tcp --dport 3030 -j ACCEPT
+    iptables -D INPUT -p tcp -m tcp --dport 3030 -j ACCEPT
 
     # add
     iptables -A INPUT -i eth0 -p tcp -m tcp --dport 443 -j ACCEPT
     iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
     iptables -A INPUT -i eth0 -p tcp -m tcp --dport 22 -j ACCEPT
     iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-    iptables -A INPUT -i eth0 -p tcp -m tcp --dport 3000 -j ACCEPT
-    iptables -A INPUT -p tcp -m tcp --dport 3000 -j ACCEPT
+    iptables -A INPUT -i eth0 -p tcp -m tcp --dport 3030 -j ACCEPT
+    iptables -A INPUT -p tcp -m tcp --dport 3030 -j ACCEPT
 fi
 
 echo "copying config files..."
