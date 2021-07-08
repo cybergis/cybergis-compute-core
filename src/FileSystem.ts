ffd406e2b42136dc0b4395ebae5e416f26f8b109
Zimo Xiao <xiaozimo@zuggr.com>
2021-07-08 15:34:53 +0800

    Update FileSystem.ts

--- src/FileSystem.ts
+++ src/FileSystem.ts
@@ -160,14 +160,14 @@ export class GitFolder extends BaseFolder {
                 var { stdout, stderr } = await exec(`git rev-parse HEAD`)
                 var sha = stdout.trim()
                 if (sha != this.config.sha) {
-                    await exec(`cd ${this.path} && git checkout HEAD`)
-                    await exec(`cd ${this.path} && git reset --hard`)
+                    await exec(`cd ${this.path} && git fetch --all`)
+                    await exec(`cd ${this.path} && git reset --hard @{u}`)
                     await exec(`cd ${this.path} && git pull`)
                     await exec(`cd ${this.path} && git checkout ${this.config.sha}`)
                 }
             } else {
-                await exec(`cd ${this.path} && git checkout HEAD`)
-                await exec(`cd ${this.path} && git reset --hard`)
+                await exec(`cd ${this.path} && git fetch --all`)
+                await exec(`cd ${this.path} && git reset --hard @{u}`)
                 await exec(`cd ${this.path} && git pull`)
             }
 
