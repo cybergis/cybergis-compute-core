28a6c2381b9a3802af327f52b86d3a0138bc67a2
Mit <53411468+mitkotak@users.noreply.github.com>
2022-05-16 08:09:57 -0500

    documented GlobusUtil.ts + XSEDEUtil.ts

--- src/lib/GlobusUtil.ts
+++ src/lib/GlobusUtil.ts
@@ -8,6 +8,9 @@ const redis = require('redis')
 const { promisify } = require("util")
 
 export class GlobusTaskListManager {
+    /**
+     * Class for managing globus tasks
+     */
     private redis = {
         getValue: null,
         setValue: null,
@@ -16,17 +19,34 @@ export class GlobusTaskListManager {
 
     private isConnected = false
 
+    /**
+     * Assigns label to taskId
+     * 
+     * @param{string} label - input label
+     * @param{string} taskId - setValue id
+     */
     async put(label: string, taskId: string) {
         await this.connect()
         await this.redis.setValue(`globus_task_${label}`, taskId)
     }
 
+    /**
+     * Get taskId for specified label
+     * 
+     * @param{string} label - input label
+     * @return{string} out - redis output
+     */
     async get(label: string): Promise<string> {
         await this.connect()
         var out = await this.redis.getValue(`globus_task_${label}`)
         return out ? out : null
     }
 
+    /**
+     * removes taskId for specified label
+     * 
+     * @param{string} label - input label
+     */
     async remove(label: string) {
         await this.connect()
         var out = await this.get(label)
@@ -34,6 +54,10 @@ export class GlobusTaskListManager {
         this.redis.delValue(`globus_task_${label}`)
     }
 
+    /**
+     * @async
+     * Connect to globus through redis
+     */
     private async connect() {
         if (this.isConnected) return
 
@@ -55,8 +79,21 @@ export class GlobusTaskListManager {
 }
 
 export default class GlobusUtil {
+    /**
+     * Class for accessing Globus commands
+     */
     static db = new DB()
 
+    /**
+     * @static
+     * Initializes globus job
+     * @param{GlobusFolder} from - from transfer folder
+     * @param{GlobusFolder} to - to transfer folder
+     * @param{hpcConfig} hpcConfig - hpcConfiguration
+     * @param{string} label - task label
+     * @throw{Error} - Thrown if globus query status fails
+     * @return{string} - taskId
+     */
     static async initTransfer(from: GlobusFolder, to: GlobusFolder, hpcConfig: hpcConfig, label: string = ''): Promise<string> {
         var connection = await this.db.connect()
         var globusTransferRefreshTokenRepo = connection.getRepository(GlobusTransferRefreshToken)
@@ -80,14 +117,41 @@ export default class GlobusUtil {
         return out['task_id']
     }
 
+    /**
+     * @static
+     * @async
+     * Returns output of querying 'globus_monitor.py'
+     * @param{string} taskId - taskId of transfer
+     * @param{hpcConfig} hpcConfig - hpcConfiguration
+     * @return{Promise<string>} - queryStatus string
+     */
     static async monitorTransfer(taskId: string, hpcConfig: hpcConfig): Promise<string> {
         return await this._queryStatus(taskId, hpcConfig, 'globus_monitor.py')
     }
 
+    /**
+     * @static
+     * @async
+     * Returns output of querying 'globus_query_status.py'
+     * @param{string} taskId - taskId of transfer
+     * @param{hpcConfig} hpcConfig - hpcConfiguration
+     * @return{Promise<string>} - queryStatus string
+     */
     static async queryTransferStatus(taskId: string, hpcConfig: hpcConfig): Promise<string> {
         return await this._queryStatus(taskId, hpcConfig, 'globus_query_status.py')
     }
 
+    
+    /**
+     * @static
+     * @async
+     * Implements the specified globus query
+     * @param{string} taskId - taskId of transfer
+     * @param{hpcConfig} hpcConfig - hpcConfiguration
+     * @param{string} script - query string
+     * @throw{Error} - thrown when Globus query status fails
+     * @return{string} - queryStatus string
+     */
     static async _queryStatus(taskId: string, hpcConfig: hpcConfig, script: string) {
         var connection = await this.db.connect()
         var globusTransferRefreshTokenRepo = connection.getRepository(GlobusTransferRefreshToken)
