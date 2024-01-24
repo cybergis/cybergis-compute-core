1e523506eb0cca2c8ddedac86c8aedcd6719230e
yianzhang14 <78504318+yianzhang14@users.noreply.github.com>
2024-01-23 22:07:26 -0600

    refactored statistic

--- src/Statistic.ts
+++ src/Statistic.ts
@@ -1,9 +1,18 @@
 import DB from "./DB";
 import { Job } from "./models/Job";
 
+/**
+ * Wrapper class for requesting statistics from the database.
+ */
 export default class Statistic {
   private db = new DB();
 
+  /**
+   * Returns the runtime of a job given its jobId.
+   *
+   * @param jobId id of the job
+   * @returns runtime of jof (initialization time - finish time)
+   */
   public async getRuntimeByJobId(jobId: string) {
     const connection = await this.db.connect();
     const statistic = await connection
@@ -17,18 +26,22 @@ export default class Statistic {
         { id: jobId }
       )
       .getRawOne();
-    
-    return statistic
 
+    return statistic;
   }
+
+  /**
+   * Requests and calculates statistics relating to total runtime--both absolute and by HPC.
+   * 
+   * @returns dictionary of results, including total and statistics by HPC
+   */
   public async getRuntimeTotal() {
     const connection = await this.db.connect();
+
     const statisticTotal = await connection
       .getRepository(Job)
       .createQueryBuilder("job")
-      .select(
-        "SUM(ABS(job.initializedAt - job.finishedAt)) as STATISTIC"
-      )
+      .select("SUM(ABS(job.initializedAt - job.finishedAt)) as STATISTIC")
       .where("job.initializedAt IS NOT NULL AND job.finishedAt IS NOT NULL")
       .getRawOne();
 
@@ -46,11 +59,13 @@ export default class Statistic {
       var out = {
         total: parseInt(statisticTotal["STATISTIC"]),
       };
+
       for (var i in statisticByHPC) {
         out[statisticByHPC[i]["HPC"]] = parseInt(
           statisticByHPC[i]["STATISTIC"]
         );
       }
+
       return out;
     } else {
       return null;
