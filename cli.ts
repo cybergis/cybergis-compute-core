c6a811a5313c1e81ee20fba0e8e056f46dd83b56
yianzhang14 <78504318+yianzhang14@users.noreply.github.com>
2024-02-08 13:37:19 -0600

    refactored cli.ts

--- cli.ts
+++ cli.ts
@@ -2,8 +2,8 @@ import { Command } from "commander";
 import { Git } from "./src/models/Git";
 import DB from "./src/DB";
 
-var pkg = require("../package.json");
-var cmd = new Command();
+const pkg = require("../package.json");  // eslint-disable-line
+const cmd = new Command();
 
 cmd.version(pkg.version);
 
@@ -20,74 +20,99 @@ cmd
   .option("-s, --sha <sha>", "[operation=add/update] git repository's sha hash")
   .action(async (operation: string, cmd) => {
     const db = new DB(false);
+
     switch (operation) {
-      case "add":
-        var git = new Git();
-        if (cmd.address && cmd.id) {
-          git.address = cmd.address;
-          git.id = cmd.id;
-        } else {
-          console.error(
-            "-a, --address <address> and -i, --id <id> flags is required"
-          );
-          return;
-        }
-        git.isApproved = true;
-        if (cmd.sha) git.sha = cmd.sha;
-        var connection = await db.connect();
-        var gitRepo = connection.getRepository(Git);
-        await gitRepo.save(git);
-        console.log("git successfully added:");
-        console.log(git);
-        break;
-      case "update":
-        if (!cmd.id) {
-          console.error("-i, --id <id> flag is required");
-          return;
-        }
-        var connection = await db.connect();
-        var i = {};
-        if (cmd.address) i["address"] = cmd.address;
-        if (cmd.sha) i["sha"] = cmd.sha;
-        await connection
-          .createQueryBuilder()
-          .update(Git)
-          .where("id = :id", { id: cmd.id })
-          .set(i)
-          .execute();
-        console.log("git successfully updated:");
-        var gitRepo = connection.getRepository(Git);
-        console.log(await gitRepo.findOne(cmd.id));
-        break;
-      case "approve":
-        if (!cmd.id) {
-          console.error("-i, --id <id> flag is required");
-          return;
-        }
-        var connection = await db.connect();
-        await connection
-          .createQueryBuilder()
-          .update(Git)
-          .where("id = :id", { id: cmd.id })
-          .set({ isApproved: true })
-          .execute();
-        console.log("git approved");
-        break;
-      case "delete":
-        if (!cmd.id) {
-          console.error("-i, --id <id> flag is required");
-          return;
-        }
-        var gitRepo = connection.getRepository(Git);
-        await gitRepo.delete(cmd.id);
-        console.log("git successfully deleted");
-        break;
-      default:
+    case "add": {
+      const git = new Git();
+
+      if (cmd.address && cmd.id) {
+        git.address = cmd.address;
+        git.id = cmd.id;
+      } else {
         console.error(
-          "<operation> invalid operation, only support [add/update/delete/approve]"
+          "-a, --address <address> and -i, --id <id> flags is required"
         );
-        break;
+        return;
+      }
+
+      git.isApproved = true;
+      if (cmd.sha) git.sha = cmd.sha;
+
+      const connection = await db.connect();
+      const gitRepo = connection.getRepository(Git);
+      await gitRepo.save(git);
+
+      console.log("git successfully added:");
+      console.log(git);
+
+      break;
     }
+    case "update": {
+      if (!cmd.id) {
+        console.error("-i, --id <id> flag is required");
+        return;
+      }
+
+      const connection = await db.connect();
+      const i = {};
+
+      if (cmd.address) i["address"] = cmd.address;
+      if (cmd.sha) i["sha"] = cmd.sha;
+
+      await connection
+        .createQueryBuilder()
+        .update(Git)
+        .where("id = :id", { id: cmd.id })
+        .set(i)
+        .execute();
+
+      console.log("git successfully updated:");
+      const gitRepo = connection.getRepository(Git);
+      console.log(await gitRepo.findOne(cmd.id));
+
+      break;
+    }
+    case "approve": {
+      if (!cmd.id) {
+        console.error("-i, --id <id> flag is required");
+        return;
+      }
+
+      const connection = await db.connect();
+      await connection
+        .createQueryBuilder()
+        .update(Git)
+        .where("id = :id", { id: cmd.id })
+        .set({ isApproved: true })
+        .execute();
+
+      console.log("git approved");
+
+      break;
+    }
+    case "delete": {
+      if (!cmd.id) {
+        console.error("-i, --id <id> flag is required");
+        return;
+      }
+
+      const connection = await db.connect();
+      const gitRepo = connection.getRepository(Git);
+      await gitRepo.delete(cmd.id);
+
+      console.log("git successfully deleted");
+
+      break;
+    }
+    default: {
+      console.error(
+        "<operation> invalid operation, only support [add/update/delete/approve]"
+      );
+
+      break;
+    }
+    }
+    
     await db.close();
   });
 
