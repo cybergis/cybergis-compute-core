9a2bd87b58e8d151d35563e61cc4e08a2c1f73f0
John Speaks <113151039+JTSIV1@users.noreply.github.com>
2024-12-09 11:04:44 -0600

    working download helper

--- src/helpers/DownloadUploadUtil.ts
+++ src/helpers/DownloadUploadUtil.ts
@@ -10,6 +10,7 @@ import FolderUtil from "./FolderUtil";
 export default class DownloadUploadUtil {
   private async exec(command: string, options: any): Promise<{ stdout: string, stderr: string }> {
     return new Promise((resolve, reject) => {
+      console.log("executing command", command);
       const child = spawn(command, { shell: true, ...options });
 
       let stdout = '';
@@ -32,54 +33,75 @@ export default class DownloadUploadUtil {
       });
 
       child.on('error', (err) => {
-        reject(err);
+        reject(new Error(`Exec failed with ${err}`));
       });
+      console.log("executed command", command);
     });
   }
 
-  private ssh(jobId: string) : SSH {
-    return connectionPool[jobId!].ssh;
+  private async ssh(hpc: string) : Promise<SSH> {
+    const connection = connectionPool[hpc].ssh;
+    if (!connection) {
+      throw new ConnectorError(`No connection found for HPC: ${hpc}`);
+    }
+    console.log("ssh connection found", connection);
+    try {
+      await Helper.runCommandWithBackoff((async (ssh: SSH) => {
+        if (!ssh.connection.isConnected()) {
+          await ssh.connection.connect(ssh.config);
+        }
+        await ssh.connection.execCommand("echo");
+      }) as callableFunction, [connection], null);
+    } catch (e) {
+      console.log("error connecting to HPC", hpc, e);
+      throw new ConnectorError(`Failed to connect to HPC: ${hpc} with error ${e}`);
+    }
+    return connection;
   }
 
   private async zip(
     from: string,
-    to: string
+    to: string,
+    hpc: string
   ): Promise<string | null> {
-    const out = await this.exec(
-      `zip -q -r ${to} . ${path.basename(from)}`,  // quiet, recursive, to to at the current directory from the from directory path
-      Object.assign(
-        {
-          cwd: from,  // set cwd to spawn child in the from directory
-        },
-        {}
-      )
-    );
-
-    return out.stdout;
+    console.log("zipping file from", from, "to", to);
+    const ssh = await this.ssh(hpc);
+    const command = `zip -q -r ${to} ${path.basename(from)}`;
+    const { stdout, stderr } = await ssh.connection.execCommand(command, { cwd: path.dirname(from) });
+    if (stderr) {
+      throw new Error(`Failed to zip file: ${stderr}`);
+    }
+    console.log("zipped file from", from, "to", to);
+    return stdout;
   }
 
   private async rm(
     path: string
   ): Promise<string | null> {
-
+    console.log("removing path", path);
     const out = await this.exec(`rm -rf ${path};`, {});
+    console.log("removed path", path);
     return out.stdout;
   }
 
-  async download(from: string, to: string, jobId: string) {
+  async download(from: string, to: string, hpc: string): Promise<void> {
+    console.log("downloading file from", from, "to", to);
     if (to === undefined)
       throw new ConnectorError("please init input file first");
 
     // create from/to zip paths from raw files and zip the from file
     const fromZipFilePath = from.endsWith(".zip") ? from : `${from}.zip`;
     const toZipFilePath = `${to}.zip`;
-    await this.zip(from, fromZipFilePath);
+    await this.zip(from, fromZipFilePath, hpc);
+
+    console.log("start download");
 
     try {      
       // try to get the from file via ssh/scp and remove the compressed folder afterwards
       // wraps command with backoff -> takes lambda function and array of inputs to execute command
       await Helper.runCommandWithBackoff.call(this, (async (to1: string, zipPath: string) => {
-        await this.ssh(jobId).connection.getFile(to1, zipPath);
+        const ssh = await this.ssh(hpc);
+        await ssh.connection.getFile(to1, zipPath);
       }) as callableFunction, [to, fromZipFilePath], "Trying to download file again");
       await this.rm(fromZipFilePath);
 
@@ -89,6 +111,6 @@ export default class DownloadUploadUtil {
       const error = `unable to get file from ${from} to ${to}: ` + Helper.assertError(e).toString();
       throw new ConnectorError(error);
     }
+    console.log("downloaded file from", from, "to", to);
   }
 }
-
