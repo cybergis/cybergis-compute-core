8b85fe18e6faf5187b49a29f19be704a9a80299b
Ian Zhang <yianzhang14@gmail.com>
2025-04-15 01:11:49 -0500

    disable all middleware

--- src/server/server.ts
+++ src/server/server.ts
@@ -51,9 +51,9 @@ async function initHelloWorldGit() {
 
 // handle parsing arguments
 // app.use(bodyParser.json());  // possibly unneeded now with newer versions of express
-app.use(express.json());
-app.use(morgan("combined"));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(morgan("combined"));
+// app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 // uploading files
