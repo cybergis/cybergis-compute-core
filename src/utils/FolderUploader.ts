40ea7782f5fdbd5669a7ca264d583d09a3b198aa
yianzhang14 <yianzhang14@gmail.com>
2025-05-01 13:16:13 -0500

    unzip on remote if the file is a zip file

--- src/utils/FolderUploader.ts
+++ src/utils/FolderUploader.ts
@@ -1,7 +1,6 @@
 
 
 import * as fs from "fs";
-import { stat } from "fs/promises";
 import * as path from "path";
 
 import { hpcConfigMap } from "../../configs/config";
@@ -16,6 +15,7 @@ import {
 import GitUtil from "../helpers/GitUtil";
 import { GlobusClient } from "../helpers/GlobusTransferUtil";
 import * as Helper from "../helpers/Helper";
+import { isDirectory, isZipped } from "../helpers/LocalFolderUtil";
 import { Cache, Folder } from "../models";
 
 import dataSource from "./DB";
@@ -217,12 +217,15 @@ async function localFolderUpload(base: BaseFolderUploader, from: LocalFolder) {
     throw new Error(`could not find folder under path ${from.localPath}`);
   }
 
-  console.log(from.localPath, base.hpcPath);
+  if (await isDirectory(from.localPath)) {
+    const remoteFilePath = path.join(base.hpcPath, path.basename(from.localPath));
+    await base.connector.mkdir(base.hpcPath);
+    await base.connector.uploadFile(from.localPath, remoteFilePath, false);
 
-  const stats = await stat(from.localPath);
-
-  if (stats.isFile()) {
-    await base.connector.uploadFile(from.localPath, base.hpcPath, false);
+    if (await isZipped(base.hpcPath)) {
+      await base.connector.unzip(remoteFilePath, base.hpcPath);
+      void base.connector.rm(remoteFilePath);
+    }
   } else {
     const zipPath = `${base.hpcPath}.zip`;
     await base.connector.uploadFolderZip(from.localPath, zipPath, false);
